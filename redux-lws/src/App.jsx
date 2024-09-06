1ba858934cb72bff08c9499e9a0614011074dac0
NoorTushar <tushar.noor.khan@gmail.com>
2024-09-06 20:52:39 +0600

    dispatch actions and show on UI

--- redux-lws/src/App.jsx
+++ redux-lws/src/App.jsx
@@ -3,12 +3,14 @@ import CounterLesson from "./CounterLesson/CounterLesson";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import Counter from "./components/Counter";
 
 function App() {
    return (
       <>
          <Provider store={store}>
-            <CounterLesson />
+            {/* <CounterLesson /> */}
+            <Counter />
          </Provider>
       </>
    );
--- redux-lws/src/redux/counter/counterReducer.js
+++ redux-lws/src/redux/counter/counterReducer.js
@@ -5,9 +5,9 @@ const initialState = { value: 0 };
 const counterReducer = (state = initialState, action) => {
    switch (action.type) {
       case INCREMENT:
-         return { ...state, value: state.value + 1 };
+         return { ...state, value: state.value + action.payload };
       case DECREMENT:
-         return { ...state, value: state.value - 1 };
+         return { ...state, value: state.value - action.payload };
       default:
          return { ...state };
    }
