428bd179c5958c84d56bcca6cb1ed03a8c5227bb
NoorTushar <tushar.noor.khan@gmail.com>
2024-09-06 23:04:18 +0600

    useDispatch Hook

--- redux-lws/src/components/HooksCounter.jsx
+++ redux-lws/src/components/HooksCounter.jsx
@@ -1,24 +1,33 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "../redux/counter/actions";
 
 function HooksCounter() {
    const count = useSelector((state) => state.value);
+   const dispatch = useDispatch();
+
+   const handleIncrement = (value) => {
+      dispatch(increment(value));
+   };
+
+   const handleDecrement = (value) => {
+      dispatch(decrement(value));
+   };
    return (
       <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
          <div className="text-2xl font-semibold">{count}</div>
          <div className="flex space-x-3">
-            {/* <button
+            <button
                className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-               onClick={increment}
+               onClick={() => handleIncrement(3)}
             >
                Increment
             </button>
             <button
                className="bg-red-400 text-white px-3 py-2 rounded shadow"
-               onClick={decrement}``
+               onClick={() => handleDecrement(3)}
             >
                Decrement
-            </button> */}
+            </button>
          </div>
       </div>
    );
