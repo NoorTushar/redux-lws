473f4990c7686525dbde02781c30ceb4ff5810eb
NoorTushar <tushar.noor.khan@gmail.com>
2024-09-06 22:42:15 +0600

    useSelector hook

--- redux-lws/src/App.jsx
+++ redux-lws/src/App.jsx
@@ -4,13 +4,15 @@ import CounterLesson from "./CounterLesson/CounterLesson";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Counter from "./components/Counter";
+import HooksCounter from "./components/HooksCounter";
 
 function App() {
    return (
       <>
          <Provider store={store}>
             {/* <CounterLesson /> */}
-            <Counter />
+            {/* <Counter /> */}
+            <HooksCounter />
          </Provider>
       </>
    );
